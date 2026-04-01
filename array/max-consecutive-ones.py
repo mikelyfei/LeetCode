696c0b3b7c15934d992ece99601df1ffea7c5528
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-03-31 22:52:18 -0700

    LC-max-consecutive-ones [Runtime: 15 ms; Memory: 21.9 MB]

--- array/max-consecutive-ones.py
+++ array/max-consecutive-ones.py
@@ -4,11 +4,13 @@ class Solution:
         left = 0
         right = 0
         res = 0
-        for left in range(n):
+        while left < n:
             if nums[left] == 0:
+                left += 1
                 continue
             right = left
             while right < n and nums[right] == 1:
                 right += 1
-                res = max(res, right-left)
+            res = max(res, right-left)
+            left = right + 1
         return res
