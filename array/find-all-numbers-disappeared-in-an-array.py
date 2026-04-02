feb05948fc9d9257b2aca680fd275e2e01a8dbc1
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-01 22:48:56 -0700

    LC-find-all-numbers-disappeared-in-an-array [Runtime: 20 ms; Memory: 30.4 MB]

--- array/find-all-numbers-disappeared-in-an-array.py
+++ array/find-all-numbers-disappeared-in-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         unique_nums = set(nums)
         res = []
-        for i in range(1, n):
+        for i in range(1, n+1):
             if i not in unique_nums:
                 res.append(i)
         return res
