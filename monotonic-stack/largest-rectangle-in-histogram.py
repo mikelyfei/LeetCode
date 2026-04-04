b80c6f121613a27474db97d35d1010436f0f7dc1
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-03 22:25:10 -0700

    LC-largest-rectangle-in-histogram [Runtime: 207 ms; Memory: 35.4 MB]

--- monotonic-stack/largest-rectangle-in-histogram.py
+++ monotonic-stack/largest-rectangle-in-histogram.py
@@ -7,9 +7,9 @@ class Solution:
         last_smaller = [-1] * n
         maxArea = 0
         for i, h in enumerate(heights):
-            while next_smaller_stack and heights[next_smaller_stack[-1]] >= h:
+            while next_smaller_stack and heights[next_smaller_stack[-1]] > h:
                 next_smaller[next_smaller_stack.pop()] = i
-            while last_smaller_stack and heights[last_smaller_stack[-1]] >= heights[n-1-i]:
+            while last_smaller_stack and heights[last_smaller_stack[-1]] > heights[n-1-i]:
                 last_smaller[last_smaller_stack.pop()] = n-1-i
             next_smaller_stack.append(i)
             last_smaller_stack.append(n-1-i)
