d64fbf7ea726d5495fed0f9533bea797344e78ee
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-03 21:28:52 -0700

    LC-daily-temperatures [Runtime: 96 ms; Memory: 28.6 MB]

---
+++ stack/daily-temperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        ans = [0] * len(temperatures)
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+        return ans 
