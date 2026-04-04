df14603e695536ca4e293d4666f345541c9c33de
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-03 20:56:58 -0700

    LC-decode-the-slanted-ciphertext [Runtime: 619 ms; Memory: 40.3 MB]

--- simulation/decode-the-slanted-ciphertext.py
+++ simulation/decode-the-slanted-ciphertext.py
@@ -3,16 +3,18 @@ class Solution:
         n = len(encodedText)
         cols = n // rows
         board = [[0] * cols for _ in range(rows)] 
+        n_letters = 0
         for i in range(rows):
             for j in range(cols):
                 board[i][j] = encodedText[i*cols + j]
+                if encodedText[i*cols + j].isalpha():
+                    n_letters += 1
         res = []
-        for j in range(cols):
-            for i in range(rows-1, 0, -1):
-                if i+j < cols and board[i][0] != " ":
-                    res.append(board[i][i+j])
+      
         for j in range(cols):
             for i in range(rows):
-                if i+j < cols and board[0][j] != " ":
+                if i+j < cols and n_letters > 0:
                     res.append(board[i][j+i])
+                    if board[i][j+i].isalpha():
+                        n_letters -= 1
         return "".join(res)
