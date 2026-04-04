8d796b974fef6a58095f8acc99cfd3511a61d31c
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-03 20:46:16 -0700

    LC-decode-the-slanted-ciphertext [Runtime: N/A; Memory: N/A]

---
+++ string/decode-the-slanted-ciphertext.py
@@ -0,0 +1,14 @@
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        n = len(encodedText)
+        cols = n // rows
+        board = [[0] * cols for _ in range(rows)] 
+        for i in range(rows):
+            for j in range(cols):
+                board[i][j] = encodedText[i*cols + j]
+        res = []
+        for j in range(cols):
+            for i in range(rows):
+                if i+j < cols and board[0][j] != " ":
+                    res.append(board[i][j+i])
+        return "".join(res)
