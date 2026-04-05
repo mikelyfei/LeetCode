c2b96cbdb153e084f1e3ff9f866ca77d78b277a7
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-04 21:30:52 -0700

    LC-construct-target-array-with-multiple-sums [Runtime: 7 ms; Memory: 25.2 MB]

--- heap-priority-queue/construct-target-array-with-multiple-sums.py
+++ heap-priority-queue/construct-target-array-with-multiple-sums.py
@@ -4,14 +4,20 @@ class Solution:
         s = sum(target)
         target = [-t for t in target]
         heapq.heapify(target)
-        while target:
-            largest = -heapq.heappop(target)
-            if largest == 1:
-                return True
+        while True:
+            largest = -heapq.heappop(target)                        
             rest_s = s - largest
-            largest -= rest_s
-            s -= rest_s
-            if largest <= 0:
+
+            if largest == 1 or rest_s == 1:
+                return True
+
+            if largest <= rest_s or rest_s == 0:
                 return False
-            heapq.heappush(target, -largest)
+            
+            prev = largest % rest_s
+            if prev == 0:
+                return False
+            s = rest_s + prev
+
+            heapq.heappush(target, -prev)
 
