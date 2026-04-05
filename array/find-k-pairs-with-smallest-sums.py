1e6335c9470c29ed4233bd39010e602c7d1620d9
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-04 20:02:32 -0700

    LC-find-k-pairs-with-smallest-sums [Runtime: 75 ms; Memory: 39.8 MB]

--- array/find-k-pairs-with-smallest-sums.py
+++ array/find-k-pairs-with-smallest-sums.py
@@ -2,11 +2,14 @@ class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         import heapq
         heap = []
-        for n1 in nums1:
-            for n2 in nums2:
-                heapq.heappush(heap, (n1+n2, n1, n2))
+        n1 = len(nums1)
+        n2 = len(nums2)
+        for i in range(min(k, n1)):
+            heapq.heappush(heap, (nums1[i]+nums2[0], i, 0))
         ans = []
-        for i in range(k):
-            s, n1, n2 = heapq.heappop(heap)
-            ans.append([n1, n2])
+        while heap and len(ans) < k:
+            _, i, j = heapq.heappop(heap)
+            ans.append([nums1[i], nums2[j]])
+            if j+1 < n2:
+                heapq.heappush(heap, (nums1[i]+nums2[j+1], i, j+1))
         return ans
