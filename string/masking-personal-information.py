01b9f828ab88486bc7fb1f29fa6b666cedd6c60b
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-04 21:59:20 -0700

    LC-masking-personal-information [Runtime: 0 ms; Memory: 19.4 MB]

--- string/masking-personal-information.py
+++ string/masking-personal-information.py
@@ -4,7 +4,7 @@ class Solution:
             name, ext = s.split("@")
             name = name[0].lower() + "*****" + name[-1].lower()
             return name + "@" + ext.lower()
-        s = s.replace("(", "").replace(")", "").replace("+", "").replace("-", "")
+        s = s.replace("(", "").replace(")", "").replace("+", "").replace("-", "").replace(" ", "")
         flag = len(s)==10
         last4 = s[-4:]
         prev = "*" * (len(s)-4)
