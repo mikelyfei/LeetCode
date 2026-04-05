7dc4ed915f3b9ef89d74418f356c6969e61eaa6f
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-04 23:38:51 -0700

    LC-two-sum [Runtime: 0 ms; Memory: 20.4 MB]

---
+++ array/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in dic:
+                return [dic[comp], i]
+            dic[num] = i
+            
