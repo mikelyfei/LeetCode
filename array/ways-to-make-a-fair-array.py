ff30f7c62bd8d904ca7e609020710476aeb16972
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-05 23:49:19 -0700

    LC-ways-to-make-a-fair-array [Runtime: 230 ms; Memory: 27.3 MB]

--- array/ways-to-make-a-fair-array.py
+++ array/ways-to-make-a-fair-array.py
@@ -1,28 +1,23 @@
 class Solution:
     def waysToMakeFair(self, nums: List[int]) -> int:
         n = len(nums)
-        if n<=1:
-            return 1
-        odd_prefix = []
-        even_prefix = []
-        op = 0
-        ep = 0
-        for i in range(n):
+        even = [0]*n
+        odd = [0]*n
+        even[0] = nums[0]
+        for i in range(1,n):
+            even[i] = even[i-1]
+            odd[i] = odd[i-1]
             if i%2:
-                op += nums[i]
-                odd_prefix.append(op)
+                odd[i]+=nums[i]
             else:
-                ep += nums[i]
-                even_prefix.append(ep)
-        ans = 0
-        len_odd = len(odd_prefix)
-        if even_prefix[-1] - nums[0] == odd_prefix[-1]:
-            ans += 1
+                even[i]+=nums[i]
+        
+        ans=0
         for i in range(n):
-            even_sum_before = even_prefix[i//2] - nums[i] if i%2==0 else even_prefix[i//2]
-            odd_sum_before = odd_prefix[min(len_odd-1, i//2)] - nums[i] if i%2 else odd_prefix[min(len_odd-1,i//2)]
-            even_sum_after = even_prefix[-1] - even_prefix[i//2]
-            odd_sum_after = odd_prefix[-1] - odd_prefix[min(len_odd-1, i//2)]
-            if even_sum_before + odd_sum_after == odd_sum_before + even_sum_after:
-                ans += 1
-        return ans
+            even_left = even[i-1] if i>0 else 0
+            odd_left = odd[i-1] if i>0 else 0
+            even_right = odd[-1] - odd[i]
+            odd_right = even[-1] - even[i]
+            if even_left + even_right == odd_left + odd_right:
+                ans+=1
+        return ans 
