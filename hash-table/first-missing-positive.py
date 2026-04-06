636832a1c9c7eebea131e4c7c61bb1a5f20fe45f
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-05 18:54:02 -0700

    LC-first-missing-positive [Runtime: N/A; Memory: N/A]

--- hash-table/first-missing-positive.py
+++ hash-table/first-missing-positive.py
@@ -1,8 +1,11 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
-        nums_set = set(nums)
+        seen = [0]*(n+1)
+        for i in range(n):
+            if nums[i] < n+1:
+                seen[nums[i]] = 1
         for i in range(1, n+1):
-            if i not in nums:
+            if not seen[i]:
                 return i
         return n+1
