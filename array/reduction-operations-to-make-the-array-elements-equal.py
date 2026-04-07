e4c0315270584eb92ba782120ecea87847ee18d6
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-06 22:16:15 -0700

    LC-reduction-operations-to-make-the-array-elements-equal [Runtime: 126 ms; Memory: 26.9 MB]

--- array/reduction-operations-to-make-the-array-elements-equal.py
+++ array/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,16 +1,10 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        from collections import Counter
-        nums.sort(reverse=True)
-        counter = Counter(nums)
-        
-        times = 0
-        for i, (k, v) in enumerate(counter.items()):
-            if i == 0:
-                largest = k
-            else:
-                counter[k] += counter[largest]
-                times += counter[largest]
-                counter[largest] = 0
-                largest = k
-        return times
+        nums.sort()
+        ans = 0
+        cnt = 0
+        for i in range(1, len(nums)):
+            if nums[i]!=nums[i-1]:
+                cnt+=1
+            ans+=cnt        
+        return ans
