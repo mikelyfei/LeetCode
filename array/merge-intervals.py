4669c98fdc6a146b0cd4357d5576625a15215c56
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-06 22:19:02 -0700

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

---
+++ array/merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans = []
+        for l, r in intervals:
+            if not ans:
+                ans.append([l, r])
+            else:
+                if l<=ans[-1][-1]:
+                    ans[-1][-1]=r
+                else:
+                    ans.append([l, r])
+        return ans
