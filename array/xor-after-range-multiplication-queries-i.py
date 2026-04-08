4f87a5d94889aeaacf25f4c2b3e39e8ca7174125
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-07 22:35:01 -0700

    LC-xor-after-range-multiplication-queries-i [Runtime: 1660 ms; Memory: 20.1 MB]

--- array/xor-after-range-multiplication-queries-i.py
+++ array/xor-after-range-multiplication-queries-i.py
@@ -3,9 +3,9 @@ class Solution:
         for q in queries:
             l, r, k, v = q
             while l<=r:
-                nums[l]=(nums[l]*v)%(10e9+7)
+                nums[l]=(nums[l]*v)%(10**9+7)
                 l+=k
         ans = 0
         for num in nums:
-            ans = int((ans^int(num))%(10e9+7))
+            ans = ans^int(num)
         return ans
