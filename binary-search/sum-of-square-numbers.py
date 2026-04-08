a8f7fb01ffa5154077e3f1bbbb22200c9d60d462
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-07 21:32:32 -0700

    LC-sum-of-square-numbers [Runtime: 86 ms; Memory: 19.3 MB]

--- binary-search/sum-of-square-numbers.py
+++ binary-search/sum-of-square-numbers.py
@@ -3,7 +3,6 @@ class Solution:
         import math
         a, b = 0, math.ceil(math.sqrt(c))
         while a<=b:
-            mid = a+(b-a)//2
             if a**2+b**2>c:
                 b-=1
             elif a**2+b**2<c:
