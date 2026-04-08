6eaafa26edbd4c7899faa373a225af437cdbd8a2
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-07 22:33:06 -0700

    LC-xor-after-range-multiplication-queries-i [Runtime: N/A; Memory: N/A]

---
+++ array/xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        for q in queries:
+            l, r, k, v = q
+            while l<=r:
+                nums[l]=(nums[l]*v)%(10e9+7)
+                l+=k
+        ans = int(0)
+        for num in nums:
+            ans ^= int(num)
+        return ans
