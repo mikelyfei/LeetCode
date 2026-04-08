865d3cf105dbbc0070392271b52aa2df8406e03d
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-07 21:31:55 -0700

    LC-sum-of-square-numbers [Runtime: 100 ms; Memory: 19.5 MB]

--- two-pointers/sum-of-square-numbers.py
+++ two-pointers/sum-of-square-numbers.py
@@ -1,12 +1,12 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         import math
-        a, b = 1, math.ceil(math.sqrt(c))
+        a, b = 0, math.ceil(math.sqrt(c))
         while a<=b:
             mid = a+(b-a)//2
-            if a**2+mid**2>c:
+            if a**2+b**2>c:
                 b-=1
-            elif a**2+mid**2<c:
+            elif a**2+b**2<c:
                 a+=1
             else:
                 return True
