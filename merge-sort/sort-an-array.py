48d209492e437da1dae3acadcca709b9c10f8b61
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-09 19:45:12 -0700

    LC-sort-an-array [Runtime: N/A; Memory: N/A]

---
+++ merge-sort/sort-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quick_sort(nums, left, right):
+            if left >= right:
+                return
+            pivot = nums[left]
+            l, r = left, right
+            while l<r:
+                while l<r and nums[r]>=pivot:
+                    r-=1
+                while l<r and nums[l]<=pivot:
+                    l+=1
+                nums[l], nums[r] = nums[r], nums[l]
+            nums[left], nums[l] = nums[l], nums[left]
+            quick_sort(nums, left, l-1)
+            quick_sort(nums, l+1, right)
+        quick_sort(nums, 0, len(nums)-1)
+        return nums
