368fcaa9c3ce0f240b871c05ffaaa727c0c42e56
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-11 16:55:13 -0700

    LC-insertion-sort-list [Runtime: 411 ms; Memory: 21.2 MB]

---
+++ sorting/insertion-sort-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-float('inf'))
+        cur = head
+        while cur:
+            nxt = cur.next
+            prev = dummy
+            while prev.next and prev.next.val < cur.val:
+                prev = prev.next
+            temp = prev.next
+            prev.next = cur
+            cur.next = temp
+            cur = nxt
+        return dummy.next
