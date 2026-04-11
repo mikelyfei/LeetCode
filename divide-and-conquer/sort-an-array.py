5f153950b80390c6fea817c3de7012bb26008e2f
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-11 15:28:39 -0700

    LC-sort-an-array [Runtime: 479 ms; Memory: 27 MB]

--- divide-and-conquer/sort-an-array.py
+++ divide-and-conquer/sort-an-array.py
@@ -1,21 +1,32 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        import random
-        def quick_sort(nums, left, right):
-            if left >= right:
-                return
-            idx = random.randint(left, right)
-            nums[left], nums[idx] = nums[idx], nums[left]
-            pivot = nums[left]
-            l, r = left, right
-            while l<r:
-                while l<r and nums[r]>=pivot:
-                    r-=1
-                while l<r and nums[l]<=pivot:
-                    l+=1
-                nums[l], nums[r] = nums[r], nums[l]
-            nums[left], nums[l] = nums[l], nums[left]
-            quick_sort(nums, left, l-1)
-            quick_sort(nums, l+1, right)
-        quick_sort(nums, 0, len(nums)-1)
-        return nums
+        def merge_sort(nums, l, r):
+            if l==r:
+                return [nums[l]]
+            mid = l + (r-l)//2
+            left = merge_sort(nums, l, mid)
+            right = merge_sort(nums, mid+1, r)
+            return merge(left, right)
+        def merge(arr1, arr2):
+            if arr1 and not arr2:
+                return arr1
+            elif not arr1 and arr2:
+                return arr2
+            elif not arr1 and not arr2:
+                return []
+            n1, n2 = len(arr1), len(arr2)
+            res = []
+            i, j = 0, 0
+            while i<n1 and j<n2:
+                if arr1[i] <= arr2[j]:
+                    res.append(arr1[i])
+                    i+=1
+                else:
+                    res.append(arr2[j])
+                    j+=1
+            if i>=n1 and j<n2:
+                res.extend(arr2[j:])
+            if j>=n2 and i<n1:
+                res.extend(arr1[i:])
+            return res
+        return merge_sort(nums, 0, len(nums)-1)
