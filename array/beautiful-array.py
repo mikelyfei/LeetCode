f6a1abe6f19b36b92e8a6e5ea205fdab37244c40
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-12 13:08:46 -0700

    LC-beautiful-array [Runtime: 0 ms; Memory: 19.3 MB]

--- array/beautiful-array.py
+++ array/beautiful-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def beautifulArray(self, n: int) -> List[int]:
-        if n==1:
-            return [1]
-        if n==2:
-            return range(1, 2)
-        odd_arr = [x*2-1 for x in self.beautifulArray((n+1)//2)]
-        even_arr = [x*2 for x in self.beautifulArray(n//2)]
+        if n <= 2:
+            return list(range(1, n + 1))
+        
+        odd_arr = [2 * x - 1 for x in self.beautifulArray((n + 1) // 2)]
+        even_arr = [2 * x for x in self.beautifulArray(n // 2)]
+        
         return odd_arr + even_arr
