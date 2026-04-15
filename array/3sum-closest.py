20280beaf583b737259ce1e672b21f232a8b7d50
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-15 11:05:46 -0700

    LC-3sum-closest [Runtime: 466 ms; Memory: 19.5 MB]

--- array/3sum-closest.py
+++ array/3sum-closest.py
@@ -4,14 +4,17 @@ class Solution:
         ans = float('inf')
         for i in range(len(nums)-2):
             two_sum_target = target - nums[i]
-            for j in range(i+1, len(nums)-1):
-                k = j+1
-                while k<len(nums):
-                    x = nums[i] + nums[j] + nums[k]
-                    if abs(x-target) < abs(ans-target):
-                        ans = x
-                    if x<target:
-                        k+=1
-                    else:
-                        return ans
+            j = i+1
+            k = len(nums) - 1
+            while j<k<len(nums):
+                x = nums[i] + nums[j] + nums[k]
+                if abs(x-target) < abs(ans-target):
+                    ans = x
+                if x<target:
+                    j+=1
+                elif x == target:
+                    return x
+                else:
+                    k -= 1
+                    
         return ans
