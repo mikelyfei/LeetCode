02221e956790b59a7ec48f1d7f4c698694ecf236
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-15 22:05:31 -0700

    LC-contains-duplicate-ii [Runtime: N/A; Memory: N/A]

--- hash-table/contains-duplicate-ii.py
+++ hash-table/contains-duplicate-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         for i in range(len(nums)-1):
-            j = i+1
-            while j<len(nums) and j-i<=k:
+            j = min(i+k, len(nums)-1)
+            while j>i:
                 if nums[i]==nums[j]:
                     return True
                 else:
-                    j+=1
+                    j-=1
         return False     
