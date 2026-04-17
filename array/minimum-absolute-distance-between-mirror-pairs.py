6085a54e076743a0ff6592d0b7e9e76b5b87ebef
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-16 22:48:32 -0700

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: 287 ms; Memory: 48.3 MB]

--- array/minimum-absolute-distance-between-mirror-pairs.py
+++ array/minimum-absolute-distance-between-mirror-pairs.py
@@ -1,16 +1,29 @@
+from typing import List
+from collections import defaultdict
+from bisect import bisect_right
+
 class Solution:
     def minMirrorPairDistance(self, nums: List[int]) -> int:
-        from collections import defaultdict
-        cnt = defaultdict(list)
-        ans = float('inf')
+        pos = defaultdict(list)
+
         for i, num in enumerate(nums):
-            cnt[num].append(i)
+            pos[num].append(i)
+
+        ans = float("inf")
+
         for i, num in enumerate(nums):
-            reversed_num = int(str(num)[::-1])
-            if reversed_num in cnt:
-                arr = cnt[reversed_num]
-                candidates = [x for x in arr if x != i]
-                j = min(candidates, key=lambda x: abs(x - i)) if candidates else None
-                if j:
-                    ans = min(ans, abs(i-j))
-        return ans if ans!=float('inf') else -1
+            rev = int(str(num)[::-1])
+
+            if rev not in pos:
+                continue
+
+            arr = pos[rev]
+
+            # 找第一个 > i 的下标，保证 mirror pair 是 (i, j), i < j
+            k = bisect_right(arr, i)
+
+            if k < len(arr):
+                j = arr[k]
+                ans = min(ans, j - i)
+
+        return ans if ans != float("inf") else -1
