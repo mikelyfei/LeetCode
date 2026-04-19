431ae3bb03a7d792694630ea81008e58468ec93a
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-19 14:39:15 -0700

    LC-simple-bank-system [Runtime: 40 ms; Memory: 49.7 MB]

---
+++ design/simple-bank-system.py
@@ -0,0 +1,36 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        balance = self.balance
+        n = len(balance)
+        if account1>n or account2>n or account1<1 or account2<1 or money > balance[account1-1]:
+            return False
+        balance[account1-1] -= money
+        balance[account2-1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        n = len(self.balance)
+        if account>n or account<1:
+            return False
+        self.balance[account-1]+=money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        n = len(self.balance)
+
+        if account>n or account<1 or self.balance[account-1]<money:
+            return False
+        self.balance[account-1]-=money
+        return True
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
