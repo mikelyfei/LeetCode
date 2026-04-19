072990c660d68cf475f71f29c2e39debe114a375
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-18 18:02:44 -0700

    LC-maximum-distance-between-a-pair-of-values [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-distance-between-a-pair-of-values.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        n1 = len(nums1)
+        n2 = len(nums2)
+        ans = - float('inf')
+        for i in range(n1):
+            l = i
+            if i > n2-1:
+                break
+            r = n2 
+            while l<r:
+                mid = (l + r) // 2
+                if nums2[mid] >= nums1[i]:
+                    l = mid + 1
+                else:
+                    r = mid
+            ans = max(ans, l - 1 - i)
+        return ans
