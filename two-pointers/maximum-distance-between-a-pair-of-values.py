78d0f76468d752c4a61fbc331aa6c0b8c3392f8a
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-18 18:03:07 -0700

    LC-maximum-distance-between-a-pair-of-values [Runtime: 367 ms; Memory: 35.7 MB]

--- two-pointers/maximum-distance-between-a-pair-of-values.py
+++ two-pointers/maximum-distance-between-a-pair-of-values.py
@@ -2,7 +2,7 @@ class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
         n1 = len(nums1)
         n2 = len(nums2)
-        ans = - float('inf')
+        ans = 0
         for i in range(n1):
             l = i
             if i > n2-1:
