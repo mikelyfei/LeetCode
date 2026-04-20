2a3a5132ccceac2b41f440dd781868f5787e4a69
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-19 18:23:15 -0700

    LC-number-of-islands [Runtime: 243 ms; Memory: 21.7 MB]

--- array/number-of-islands.py
+++ array/number-of-islands.py
@@ -1,20 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        m = len(grid)
+        n = len(grid[0])
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         def dfs(i, j):
-            if not 0<=i<n or not 0<=j<m or grid[i][j] == '0':
-                return 
-            grid[i][j] = '0'
-            for di, dj in dirs:
-                ni, nj = i + di, j + dj
-                dfs(ni, nj)
-        count = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == '1':
-                    count += 1
-                    dfs(i, j)
-        return count
-            
+            if i<0 or j<0 or i>=m or j>=n or grid[i][j]=="0":
+                return 0
+            grid[i][j] = "0"
+            for dx, dy in dirs:
+                x, y = i + dx, j + dy
+                dfs(x, y)
+            return 1
+        cnt = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    cnt += dfs(i, j)
+        return cnt
