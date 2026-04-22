8763d410735725188eb580ded16804e0cdd00367
Mike Lu <147356944+mikelyfei@users.noreply.github.com>
2026-04-21 21:01:48 -0700

    LC-words-within-two-edits-of-dictionary [Runtime: N/A; Memory: N/A]

---
+++ array/words-within-two-edits-of-dictionary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        ans = []
+        for q in queries:
+            for d in dictionary:
+                if len(q)!=len(d):
+                    continue
+                edits = 0
+                n = len(q)
+                for i in range(n):
+                    if q[i]!=d[i]:
+                        edits+=1
+                    if edits>2:
+                        break
+                if edits<=2:
+                    ans.append(q)
+        return ans
+        
